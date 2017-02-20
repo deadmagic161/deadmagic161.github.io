21cf68b1ad171aede79459c6e9180f15723dfb70
deadmagic161 <fernando093@yandex.ru>
2017-02-20 22:40:53 +0700

    add build

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+   entry: path.join(__dirname, 'src/js', 'app.js'),
+   output: {
+      path: 'build/js',
+      filename: 'bundle.js',
+   },
+   module: {
+      loaders: [
+         { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      ],
+   },
+}; 
