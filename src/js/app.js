32a31bb20d7840ca3d21222cf43bab07878f26bb
deadmagic161 <fernando093@yandex.ru>
2017-02-22 21:14:34 +0700

    AddBuilds

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import {render} from 'react-dom';
+import List from './list';
 
 render(
-	<h1>Hello World!</h1>,
+	<div>
+		<List/>
+		<h1>Hello World!</h1>
+	</div>,	
 	document.getElementById('root')
 );
